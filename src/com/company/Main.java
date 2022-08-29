a493db8a0ce2fd271fc2c1dc13cc7beb2165491b
azast <petteri.hokka@icloud.com>
2022-08-29 15:35:59 +0300

    All exercises added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,41 @@ public class Main {
     public static void main(String[] args) {
 
 
+        //HOMEWORK 7
+
+    boolean aa = false;
+    boolean bb = false;
+
+    if((aa == true && bb == true) || (bb == false && aa == false)){
+        System.out.println("Homework 7: true");
+    }
+    else
+        System.out.println("Homework 7: false");
+
+
+
+        //HOMEWORK 6
+    int number1 = 10;
+
+    if(number1 > 1){
+        for(int p = 2; p<number1; p = p + 1 ){
+            if(number1%p == 0){
+                System.out.println("Homework 5: Not a prime number");
+                break;
+            }
+            else
+                System.out.println("Homework 5: A prime number");
+                break;
+        }
+        if(number1 == 2){
+            System.out.println("Homework 5: A prime number");
+        }
+    }
+    else
+        System.out.println("Homework 5: Not a prime number");
+
+
+
         //HOMEWORK 4
     boolean found = false;
     int number = 1;
